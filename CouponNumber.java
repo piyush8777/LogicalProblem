f3670d7552b588de5a72947f027d36ccf50c3ee6
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-04 07:37:25 +0530

    FinalCommit

---
+++ CouponNumber.java
@@ -0,0 +1,20 @@
+
+public class CouponNumber {
+	public static void main(String[] args) {
+		char[] chars = "abcdefghijklmnop".toCharArray();
+		int max = 100000000;
+		double d = Math.random();
+		int random = (int) (d * max);
+		String sb = "";
+		System.out.println("random double " + d + " random at start " + random);
+		while (random > 0) {
+			sb = sb + (chars[random % chars.length]);
+			random /= chars.length;
+			System.out.println(random + "  " + random % chars.length + " sb is " + sb);
+		}
+
+		String couponCode = sb.toString();
+		System.out.println("Coupon Code: " + couponCode);
+	}
+
+}
